7f1e9316f1d7c95cda182edc8a2be171673e508b
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-08-09 16:47:39 -0500

    Add swagger - refactor network

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const response = require("../../../network/response");
 const Controller = require("./index");
 
-router.get("/", (req, res) => {
+router.get("/", list);
+router.get("/:id", get);
+router.post("/", upsert);
+router.put("/", upsert);
+
+function list(req, res) {
   Controller.list()
     .then((lista) => {
       response.success(req, res, lista, 200);
@@ -11,17 +16,26 @@ router.get("/", (req, res) => {
     .catch((err) => {
       response.error(req, res, err.message, 500);
     });
-});
+}
 
-router.get("/:id", (req, res) => {
+function get(req, res) {
   Controller.get(req.params.id)
     .then((user) => {
-        response.success(req, res, user, 200);
+      response.success(req, res, user, 200);
     })
     .catch((err) => {
-        response.error(req, res, err.message, 500);
+      response.error(req, res, err.message, 500);
     });
-  
-});
+}
+
+function upsert(req, res) {
+  Controller.upsert(req.body)
+    .then((user) => {
+      response.success(req, res, user, 200);
+    })
+    .catch((err) => {
+      response.error(req, res, err.message, 500);
+    });
+}
 
 module.exports = router;
