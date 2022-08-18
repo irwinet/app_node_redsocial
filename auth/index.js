6e3c928b40c57e6df0021807c7b9e366cdc969c2
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-08-17 19:39:14 -0500

    Gestion de errores Throw

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config');
+const error = require('../utils/error')
 
 const secret = config.jwt.secret;
 
@@ -16,7 +17,8 @@ const check = {
         const decoded = decodeHeader(req);
         console.log(decoded)
         if(decoded.id !== owner){
-            throw new Error("No puedes realizar esta accion")
+            throw new error("No puedes realizar esta accion", 401)
+            // throw new Error("No puedes realizar esta accion")
         }
     }
 }
