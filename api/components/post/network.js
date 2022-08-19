216695b9e418118bc94fb55d62f312aa6ba3e635
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-08-18 20:44:02 -0500

    Add route posts

---
+++ api/components/post/network.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const response = require("../../../network/response");
+const Controller = require("./index");
+
+router.get("/", list);
+
+function list(req, res, next){
+    Controller.list()
+        .then(data => {
+            response.success(req, res, data,200)
+        })
+        .catch(next)
+}
+
+module.exports = router
