a058712ac391f87fa9bf05a7694183a51a093766
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-08-20 16:33:37 -0500

    Add redis api

--- config.js
+++ config.js
@@ -18,5 +18,15 @@ module.exports = {
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || 'localhost',
         port: process.env.MYSQL_SRV_PORT || 3001        
+    },
+    cacheService: {
+        host: process.env.REDIS_SRV_HOST || 'localhost',
+        port: process.env.REDIS_SRV_PORT || 3003        
+    },
+    redis: {
+        host: process.env.REDIS_HOST || 'redis-14094.c13.us-east-1-3.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || '14094',
+        user: process.env.REDIS_USER || 'default',
+        password: process.env.REDIS_PASS || 'wkvOZKGD5VOjHbJtT44PexrSRFOqZMhj'
     }
 }
